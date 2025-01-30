1750ba33106d88c92883c1adcfc0248c5dbfc5e4
Juan Martin <juanmartinmonti4@gmail.com>
2025-01-30 14:53:58 -0300

    Añadir componentes de redes sociales y actualizar configuración de Astro

---
+++ src/lib/data.ts
@@ -0,0 +1,13 @@
+export interface SocialInfo {
+  UserName: string;
+  SocialLink: string | URL;
+  SocialNetwork: string;
+}
+
+export const PersonalSocialLinks: SocialInfo[] = [
+  {
+    UserName: "jmonti51",
+    SocialLink: "",
+    SocialNetwork: "Instagram",
+  },
+];
