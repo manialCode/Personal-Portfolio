bceaa5eae42507eba990f727bb28fca743f81ff8
Juan Martin Monti <juanmartinmonti4@gmail.com>
2025-01-31 11:00:20 -0300

    Actualizar enlaces de redes sociales, mejorar accesibilidad y ajustar estilos en componentes

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -7,7 +7,17 @@ export interface SocialInfo {
 export const PersonalSocialLinks: SocialInfo[] = [
   {
     UserName: "jmonti51",
-    SocialLink: "",
+    SocialLink: "https://www.instagram.com/jmonti51/",
     SocialNetwork: "Instagram",
+  },
+  {
+    UserName: "manialCode",
+    SocialLink: "https://github.com/manialCode",
+    SocialNetwork: "GitHub",
+  },
+  {
+    UserName: "Juan Martín Monti",
+    SocialLink: "https://www.linkedin.com/in/juan-mart%C3%ADn-monti-536a98203/",
+    SocialNetwork: "LinkedIn",
   },
 ];
