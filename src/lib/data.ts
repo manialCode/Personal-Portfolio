c1659c06c635fda04a41b2a37f26065a0288b590
Juan Martin Monti <juanmartinmonti4@gmail.com>
2025-02-03 22:21:45 -0300

    Añadir interfaz y datos de proyectos: incluir información sobre proyectos y sus detalles

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -21,3 +21,34 @@ export const PersonalSocialLinks: SocialInfo[] = [
     SocialNetwork: "LinkedIn",
   },
 ];
+
+export interface Projects {
+  title: string;
+  description: String;
+  link: string | URL;
+  github: string | URL;
+  image: string | URL;
+  tags: string[];
+}
+
+export const PROJECTS: Projects[] = [
+  {
+    title: "Spotify clon",
+    description:
+      "Un clon funcional de la interfaz de Spotify construido con Astro v5, integrando React, TypeScript, y Zustand para el manejo del estado global. Este proyecto emula funcionalidades básicas de la plataforma de streaming musical, con un diseño interactivo.",
+    link: "https://spotify-clon-astro-5.vercel.app/",
+    github: "https://github.com/manialCode/Spotify-clon-Astro-5",
+    image:
+      "https://github.com/manialCode/Spotify-clon-Astro-5/blob/main/screenshots/homepage.png?raw=true",
+    tags: ["Astro", "React", "Tailwind", "Zustand", "TypeScript"],
+  },
+  {
+    title: "PDF_TALKER",
+    description:
+      "Este proyecto es una aplicación web construida con Astro que permite a los usuarios subir archivos PDF y procesarlos utilizando Cloudinary. La aplicación también utiliza Svelte para la interfaz de usuario y Tailwind CSS para el diseño.",
+    link: "",
+    github: "https://github.com/manialCode/PDF_TALKER",
+    image: "",
+    tags: ["Astro", "Svelte", "Tailwind", "TypeScript", "OpenAI"],
+  },
+];
